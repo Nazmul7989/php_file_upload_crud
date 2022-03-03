28b6fe4359f2060c8b8504020e3187dcb773abb8
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-03-03 14:47:28 +0600

    project initialize

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbHost = 'localhost';
+$dbUser = 'root';
+$dbPassword = '';
+$dbName = 'php_crud';
+
+$connection = mysqli_connect($dbHost,$dbUser,$dbPassword,$dbName);
+
+//if ($connection == true) {
+//    echo 'Connection successful';
+//}else{
+//    echo 'Connection failed';
+//}
+
+
+
+
+?>
